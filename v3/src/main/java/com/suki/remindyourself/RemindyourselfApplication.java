46d1c0b7f494773ef583c39f5c080d9e50036740
Summerki <3136753204@qq.com>
2020-02-28 15:40:56 +0800

    页面原型设计-基本完成login.html

---
+++ v3/src/main/java/com/suki/remindyourself/RemindyourselfApplication.java
@@ -0,0 +1,13 @@
+package com.suki.remindyourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindyourselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindyourselfApplication.class, args);
+    }
+
+}
---
+++ v3/src/test/java/com/suki/remindyourself/RemindyourselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suki.remindyourself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindyourselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
